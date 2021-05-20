6799a6ea67b6b79c9afc7fa06b349fa8955ee2fc
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 09:37:31 +0100

    mama

---
+++ julian/src/julian/Julio.java
@@ -0,0 +1,26 @@
+package julian;
+
+
+
+public class Julio {
+
+	private static final int quepena = 0;
+	private static final int numPartidas = 0;
+	private static final String MasterMind = null;
+	boolean visualizarSalida(boolean acertada, int i, String newParam) {
+		System.out.println ();
+		if (acertada) {
+			System.out.println (i + (i-1) +" intentos en la partida nº " +"pepino"+ numPartidas);
+		} else {
+			System.out.println (quepena + numPartidas);
+		}
+		return acertada;
+	}
+	public static boolean main(String[] args) {
+		return false;
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
